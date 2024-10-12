0a797e94fbd158dd79918cfc575a9cf585e7df68
Bakenbard05 <83641135+Bakenbard05@users.noreply.github.com>
2024-10-12 22:11:36 +0400

    Updated dllmain

--- LoaderFiles/loader/code.cpp
+++ LoaderFiles/loader/code.cpp
@@ -3,13 +3,20 @@
 #include <windows.h>
 #include <iostream>
 #include <mono/jit/jit.h>
-#define ASSEMBLY_PATH "/SceneNameGuiMod.dll"
+#include <fstream>
+#include <iostream>
+#include <mono/metadata/metadata.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/image.h>
+#include <mono/metadata/object.h>
+#include <mono/metadata/class.h>
+#include <mono/metadata/object-forward.h>
+#define ASSEMBLY_PATH "/RocketScienceSampleMod.dll"
+
 #define PAYLOAD_NAMESPACE "RSM" // Change to your namespace (This will depend on what your namespace in your C# code is)
 #define PAYLOAD_CLASS "Loader" // Leave same
 #define PAYLOAD_MAIN "Init" // Leave same
 #define MONO_DLL L"mono-2.0-bdwgc.dll" // Leave same
-
-
 
 // typedefs and fields for required mono functions
 typedef void(__cdecl* t_mono_thread_attach)(MonoDomain*);
@@ -29,7 +36,29 @@ t_mono_runtime_invoke fnRuntimeInvoke;
 typedef const char* (__cdecl* t_mono_assembly_getrootdir)(void);
 t_mono_assembly_getrootdir fnGetRootDir;
 
+
+typedef void (*t_mono_assembly_close)(MonoAssembly*);
+typedef MonoClass* (__cdecl* t_mono_object_get_class)(MonoObject*);
+typedef const char* (__cdecl* t_mono_class_get_name)(MonoClass*);
+typedef const char* (__cdecl* t_mono_class_get_namespace)(MonoClass*);
+typedef char* (__cdecl* t_mono_string_to_utf8)(MonoString*);
+typedef void(__cdecl* t_mono_free)(void*);
+typedef void(__cdecl* t_mono_string_free)(char*);
+
+t_mono_assembly_close fnMonoAssemblyClose;
+t_mono_object_get_class fnObjectGetClass;
+t_mono_class_get_name fnClassGetName;
+t_mono_class_get_namespace fnClassGetNamespace;
+t_mono_string_to_utf8 fnStringToUtf8;
+t_mono_free fnMonoFree;
+t_mono_string_free fnStringFree;
+
+MonoDomain* domain = nullptr; // Глобальная переменная для хранения домена
+MonoClass* klass = nullptr;
+MonoAssembly* assembly = nullptr;
+
 void initMonoFunctions(HMODULE mono) {
+	fnMonoAssemblyClose = (t_mono_assembly_close)GetProcAddress(mono, "mono_assembly_close");
 	fnThreadAttach = (t_mono_thread_attach)GetProcAddress(mono, "mono_thread_attach");
 	fnGetRootDomain = (t_mono_get_root_domain)GetProcAddress(mono, "mono_get_root_domain");
 	fnAssemblyOpen = (t_mono_assembly_open)GetProcAddress(mono, "mono_assembly_open");
@@ -38,16 +67,19 @@ void initMonoFunctions(HMODULE mono) {
 	fnMethodFromName = (t_mono_class_get_method_from_name)GetProcAddress(mono, "mono_class_get_method_from_name");
 	fnRuntimeInvoke = (t_mono_runtime_invoke)GetProcAddress(mono, "mono_runtime_invoke");
 	fnGetRootDir = (t_mono_assembly_getrootdir)GetProcAddress(mono, "mono_assembly_getrootdir");
+	fnObjectGetClass = (t_mono_object_get_class)GetProcAddress(mono, "mono_object_get_class");
+	fnClassGetName = (t_mono_class_get_name)GetProcAddress(mono, "mono_class_get_name");
+	fnClassGetNamespace = (t_mono_class_get_namespace)GetProcAddress(mono, "mono_class_get_namespace");
+	fnStringToUtf8 = (t_mono_string_to_utf8)GetProcAddress(mono, "mono_string_to_utf8");
+	fnMonoFree = (t_mono_free)GetProcAddress(mono, "mono_free");
+	fnStringFree = (t_mono_string_free)GetProcAddress(mono, "mono_string_free");
 }
 
 void InjectMonoAssembly() {
 	std::string assemblyDir;
 
 	HMODULE mono;
-	MonoDomain* domain;
-	MonoAssembly* assembly;
 	MonoImage* image;
-	MonoClass* klass;
 	MonoMethod* method;
 
 	/* grab the mono dll module */
@@ -83,8 +115,13 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		InjectMonoAssembly();
 		break;
 	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
 		break;
+	case DLL_PROCESS_DETACH:{
+		
+		
+		fnMonoAssemblyClose(assembly);
+		break;
+	}
 	}
 	return TRUE;
 }
--- LoaderFiles/loader/dllmain.cpp
+++ LoaderFiles/loader/dllmain.cpp
@@ -1,9 +1,8 @@
-﻿#define PAYLOAD_NAMESPACE "RSM" // Change to your namespace (This will depend on what your namespace in your C# code is)
+﻿
+#define PAYLOAD_NAMESPACE "RSM" // Change to your namespace (This will depend on what your namespace in your C# code is)
 #define PAYLOAD_CLASS "Loader" // Leave same
 #define PAYLOAD_MAIN "Init" // Leave same
 #define MONO_DLL L"mono-2.0-bdwgc.dll" // Leave same
-
-
 
 // typedefs and fields for required mono functions
 typedef void(__cdecl* t_mono_thread_attach)(MonoDomain*);
@@ -23,7 +22,29 @@ t_mono_runtime_invoke fnRuntimeInvoke;
 typedef const char* (__cdecl* t_mono_assembly_getrootdir)(void);
 t_mono_assembly_getrootdir fnGetRootDir;
 
+
+typedef void (*t_mono_assembly_close)(MonoAssembly*);
+typedef MonoClass* (__cdecl* t_mono_object_get_class)(MonoObject*);
+typedef const char* (__cdecl* t_mono_class_get_name)(MonoClass*);
+typedef const char* (__cdecl* t_mono_class_get_namespace)(MonoClass*);
+typedef char* (__cdecl* t_mono_string_to_utf8)(MonoString*);
+typedef void(__cdecl* t_mono_free)(void*);
+typedef void(__cdecl* t_mono_string_free)(char*);
+
+t_mono_assembly_close fnMonoAssemblyClose;
+t_mono_object_get_class fnObjectGetClass;
+t_mono_class_get_name fnClassGetName;
+t_mono_class_get_namespace fnClassGetNamespace;
+t_mono_string_to_utf8 fnStringToUtf8;
+t_mono_free fnMonoFree;
+t_mono_string_free fnStringFree;
+
+MonoDomain* domain = nullptr; // Глобальная переменная для хранения домена
+MonoClass* klass = nullptr;
+MonoAssembly* assembly = nullptr;
+
 void initMonoFunctions(HMODULE mono) {
+	fnMonoAssemblyClose = (t_mono_assembly_close)GetProcAddress(mono, "mono_assembly_close");
 	fnThreadAttach = (t_mono_thread_attach)GetProcAddress(mono, "mono_thread_attach");
 	fnGetRootDomain = (t_mono_get_root_domain)GetProcAddress(mono, "mono_get_root_domain");
 	fnAssemblyOpen = (t_mono_assembly_open)GetProcAddress(mono, "mono_assembly_open");
@@ -32,16 +53,19 @@ void initMonoFunctions(HMODULE mono) {
 	fnMethodFromName = (t_mono_class_get_method_from_name)GetProcAddress(mono, "mono_class_get_method_from_name");
 	fnRuntimeInvoke = (t_mono_runtime_invoke)GetProcAddress(mono, "mono_runtime_invoke");
 	fnGetRootDir = (t_mono_assembly_getrootdir)GetProcAddress(mono, "mono_assembly_getrootdir");
+	fnObjectGetClass = (t_mono_object_get_class)GetProcAddress(mono, "mono_object_get_class");
+	fnClassGetName = (t_mono_class_get_name)GetProcAddress(mono, "mono_class_get_name");
+	fnClassGetNamespace = (t_mono_class_get_namespace)GetProcAddress(mono, "mono_class_get_namespace");
+	fnStringToUtf8 = (t_mono_string_to_utf8)GetProcAddress(mono, "mono_string_to_utf8");
+	fnMonoFree = (t_mono_free)GetProcAddress(mono, "mono_free");
+	fnStringFree = (t_mono_string_free)GetProcAddress(mono, "mono_string_free");
 }
 
 void InjectMonoAssembly() {
 	std::string assemblyDir;
 
 	HMODULE mono;
-	MonoDomain* domain;
-	MonoAssembly* assembly;
 	MonoImage* image;
-	MonoClass* klass;
 	MonoMethod* method;
 
 	/* grab the mono dll module */
@@ -77,8 +101,13 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		InjectMonoAssembly();
 		break;
 	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
 		break;
+	case DLL_PROCESS_DETACH:{
+		
+		
+		fnMonoAssemblyClose(assembly);
+		break;
+	}
 	}
 	return TRUE;
 }
